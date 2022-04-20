e1a5905625d7654f261f7ffc9b686253162b80a4
Harry <xiaohanwu12@gmail.com>
2022-04-20 19:06:26 -0400

    [Fix] Output empty files

--- src/main/java/user/UserDriver.java
+++ src/main/java/user/UserDriver.java
@@ -20,6 +20,7 @@ public class UserDriver {
         job.setMapperClass(UserProfilingMapper.class);
         job.setCombinerClass(UserProfilingReducer.class);
         job.setReducerClass(UserProfilingReducer.class);
+        job.setNumReduceTasks(1);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(UserProfilingTuple.class);
@@ -36,6 +37,7 @@ public class UserDriver {
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         job.setMapperClass(UserCleaningMapper.class);
+        job.setNumReduceTasks(1);
 
         job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(UserProfilingTuple.class);
